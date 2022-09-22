1c31cf03fdaf3455d4ee3c72596d8f1fa4159bdc
Kofi <otofitatu@gmail.com>
2022-09-22 12:41:56 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navigation from './components/navigation/navigation.component';
+import Logo from './components/logo/logo.component';
+
+function App() {
+  return (
+    <div className="App">
+     <Navigation />
+     <Logo />
+     {/* <ImageLinkForm />
+     <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
